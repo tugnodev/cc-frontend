2cb8f0731da696e79f3f30d432a1f97e9906f711
tugnodev <tugnodev@gmail.com>
2025-11-23 19:15:59 +0000

    dev:auth pages

---
+++ src/lib/backend.ts
@@ -0,0 +1,41 @@
+export class backendFetch {
+  private url: string = "http://localhost:3000";
+
+  async post<T>(endPoint: string, data: T) {
+    return await fetch(`${this.url}${endPoint}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((response) => response.json());
+  }
+
+  async get<T>(endPoint: string): Promise<T> {
+    return await fetch(`${this.url}${endPoint}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => response.json());
+  }
+
+  async patch<T>(endPoint: string, data: T) {
+    return await fetch(`${this.url}${endPoint}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((response) => response.json());
+  }
+
+  async delete<T>(endPoint: string): Promise<T> {
+    return await fetch(`${this.url}${endPoint}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => response.json());
+  }
+}
