c5e2f565ab2aa49b276ec89a719829fecfd94273
ebeydi <ebeydif@gmail.com>
2025-11-24 17:49:43 +0000

    Update +page.svelte

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,31 +1,31 @@
 <script>
 </script>
 
+
+
 <div class="min-h-screen bg-base-200 flex items-center justify-center p-4">
-  <div class="card w-full max-w-md bg-base-100 shadow-xl p-6">
+  <div class="card w-full max-w-md bg-base-100 shadow-xl p-6 border border-base-300">
     <h2 class="text-3xl font-bold text-center mb-6">Connecte Toi</h2>
 
     <div class="form-control mb-4">
-      <!-- svelte-ignore a11y_label_has_associated_control -->
       <label class="label">
         <span class="label-text">Email</span>
       </label>
       <input
         type="email"
         placeholder="Entrez votre email"
-        class="input input-bordered w-full"
+        class="input input-bordered w-full border-base-300"
       />
     </div>
 
     <div class="form-control mb-2">
-      <!-- svelte-ignore a11y_label_has_associated_control -->
       <label class="label">
         <span class="label-text">Mot de passe</span>
       </label>
       <input
         type="password"
         placeholder="Entrez votre mot de passe"
-        class="input input-bordered w-full"
+        class="input input-bordered w-full border-base-300"
       />
     </div>
 
@@ -45,18 +45,15 @@
     <div class="divider">Ou connectez-vous avec</div>
 
     <div class="flex justify-center gap-4 my-4">
-      <!-- svelte-ignore a11y_consider_explicit_label -->
-      <!-- svelte-ignore a11y_consider_explicit_label -->
-      <button class="btn btn-circle"
-        ><img src="/google.svg" alt="Logo Google" class="w-5" /></button
-      >
-      <!-- svelte-ignore a11y_consider_explicit_label -->
-      <button class="btn btn-circle"
-        ><img src="/facebook.svg" alt="Logo Facebook" class="w-5" /></button
-      >
-      <button class="btn btn-circle"
-        ><img src="/apple.svg" alt="Logo Apple" class="w-6" /></button
-      >
+      <button class="btn btn-circle border-base-300">
+        <img src="/google.svg" alt="Logo Google" class="w-5" />
+      </button>
+      <button class="btn btn-circle border-base-300">
+        <img src="/facebook.svg" alt="Logo Facebook" class="w-5" />
+      </button>
+      <button class="btn btn-circle border-base-300">
+        <img src="/apple.svg" alt="Logo Apple" class="w-6" />
+      </button>
     </div>
 
     <p class="text-center">
