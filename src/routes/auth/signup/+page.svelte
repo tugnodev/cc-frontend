3e6142c8f3ad190c87875b9037a60dbf527a140a
devHack23 <pentesterdev3@gmail.com>
2025-11-30 18:53:46 +0000

    update

--- src/routes/auth/signup/+page.svelte
+++ src/routes/auth/signup/+page.svelte
@@ -1,17 +1,9 @@
 <script>
     import SignupF from "../../../components/SignupF.svelte";
-<<<<<<< HEAD
-</script>
-
-<main>
-    <SignupF />
-=======
-    import SignupForm from "../../../components/SignupForm.svelte";
 </script>
 
 <main>
     <div>
         <SignupF />
     </div>
->>>>>>> 5625999 (New update)
 </main>
