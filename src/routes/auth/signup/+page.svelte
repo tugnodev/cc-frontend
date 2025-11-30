ce24e98d42086eb82481d36f33b6a315ca0509ff
devHack23 <pentesterdev3@gmail.com>
2025-11-30 17:53:05 +0000

    false

--- src/routes/auth/signup/+page.svelte
+++ src/routes/auth/signup/+page.svelte
@@ -1,28 +1,7 @@
 <script>
-<<<<<<< HEAD
-    import SignupForm from "../../../components/SignupForm.svelte";
-</script>
-
-<main>
-    <div>
-        <section>
-            <SignupForm />
-        </section>
-    </div>
-=======
-    import { onMount } from "svelte";
-    import SignupForm from "../../../components/SignupF.svelte";
     import SignupF from "../../../components/SignupF.svelte";
-
-    let email = "";
-    let password = "";
-
-    onMount(() => {
-        console.log("Signup page mounted");
-    });
 </script>
 
 <main>
     <SignupF />
->>>>>>> be56b90 (signup)
 </main>
