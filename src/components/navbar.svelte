f15a523603094e01e1e094690711f533766ce097
tugnodev <tugnodev@gmail.com>
2025-11-30 17:37:31 +0000

    update route

---
+++ src/components/navbar.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { House, Search, CircleUserRound } from "@lucide/svelte";
+    import { page } from "$app/stores";
+
+    const navItems = [
+        { label: "Home", href: "/market", icon: House },
+        { label: "Recherche", href: "/market/search", icon: Search },
+        { label: "Profile", href: "/market/contact", icon: CircleUserRound },
+    ];
+</script>
+
+<nav class="flex bg-base-300/50 rounded-4xl">
+    <ul class="flex p-4 gap-8">
+        {#each navItems as item}
+            <li>
+                <a href={item.href}>
+                    <item.icon size={40} />
+                </a>
+            </li>
+        {/each}
+    </ul>
+</nav>
---
+++ src/routes/market/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Navbar from "../../components/navbar.svelte";
+
+    let { children } = $props();
+    import "../../app.css";
+</script>
+
+<main class="w-full h-screen items-center flex flex-col fixed z-0 bg-base-200">
+    {@render children()}
+    <div class="flex absolute z-30 bottom-6">
+        <Navbar />
+    </div>
+</main>
