16ba2067823312c5158d1f892c1329d47919c582
ebeydi <ebeydif@gmail.com>
2025-11-30 18:32:42 +0000

    nice

--- src/routes/auth/signup/+page.svelte
+++ src/routes/auth/signup/+page.svelte
@@ -1,8 +1,5 @@
 <script>
-<<<<<<< HEAD
-=======
     import SignupF from "../../../components/SignupF.svelte";
->>>>>>> 56259997f2682750efe74b877b7f47a0ffc0a93c
     import SignupForm from "../../../components/SignupForm.svelte";
 </script>
 
