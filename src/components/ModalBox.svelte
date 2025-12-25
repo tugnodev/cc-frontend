2bdb7f79eb115b403878fb37d48e708b04e61d8f
tugnodev <tugnodev@gmail.com>
2025-12-25 21:41:51 +0000

    Update ModalBox.svelte
    Co-authored-by: Sira DIENG <siradieng9305@gmail.com>

--- src/components/ModalBox.svelte
+++ src/components/ModalBox.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-    class="w-96 h-96 bg-base-200/90 rounded-2xl border border-base-300 backdrop-blur-2xl"
+    class="max-w-96 w-full min-h-96 bg-base-200/90 rounded-2xl border border-base-300 backdrop-blur-2xl flex flex-col items-center justify-center"
 >
     {@render children()}
 </div>
