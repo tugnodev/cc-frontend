5a09c1264a3f9062a43d6756569643bfda1d2c0a
devHack23 <pentesterdev3@gmail.com>
2025-12-27 23:32:02 +0100

    certifie

--- src/routes/market/+page.svelte
+++ src/routes/market/+page.svelte
@@ -1,10 +1,12 @@
 <script>
     import Main from "../../components/Main.svelte";
     import AdBanner from "../../components/AdBanner.svelte";
+    import ProfileCertife from "../../components/ProfileCertife.svelte";
 </script>
 
 <Main>
     <AdBanner images={["/ads/ad1.jpg", "/ads/ad2.jpg", "/ads/ad3.jpg"]} />
+    <ProfileCertife />
 
     <h1>Welcome to CC</h1>
 </Main>
