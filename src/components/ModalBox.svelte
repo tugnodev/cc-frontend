430c607d7b3e22d87650ea037140506a1e2a1a10
DevproCode <diamankamadou9@gmail.com>
2026-01-11 20:33:53 +0000

    devprocode

--- src/components/ModalBox.svelte
+++ src/components/ModalBox.svelte
@@ -8,10 +8,6 @@
 </script>
 
 <div
-<<<<<<< Updated upstream
-    in:blur={{ amount: 15, duration: 250 }}
-    class="absolute inset-0 p-4 bg-base-100/20 backdrop-blur z-50 flex items-center justify-center"
-=======
     role="button"
     tabindex="0"
     onclick={() => onClose()}
@@ -22,28 +18,11 @@
     }}
     in:blur={{ amount: 100, duration: 300 }}
     class="absolute inset-0 p-2 bg-base-100/20 backdrop-blur z-50 w-full h-screen flex items-center justify-center"
->>>>>>> Stashed changes
 >
     <div
-        in:blur={{ duration: 100, delay: 100 }}
+        in:blur={{ duration: 250, delay: 150 }}
         class="absolute max-w-96 w-full min-h-96 max-h-10/12 gap-2 bg-base-200/90 rounded-2xl border border-base-300 backdrop-blur-2xl flex flex-col items-center justify-start"
     >
-<<<<<<< Updated upstream
-        <div
-            class="w-full flex flex-row-reverse p-1 border-b border-base-300/50"
-        >
-            <button
-                onclick={onClose}
-                class="btn btn-md btn-soft btn-square btn-error rounded-full"
-            >
-                <X />
-            </button>
-        </div>
-        <div class="overflow-scroll w-full h-full">
-            {@render children()}
-        </div>
-=======
         {@render children()}
->>>>>>> Stashed changes
     </div>
 </div>
