48ecb2fa127890ceb47416e323af408ec956870a
devHack23 <pentesterdev3@gmail.com>
2026-01-11 19:24:55 +0000

    nouveau

--- src/routes/Shop/+page.svelte
+++ src/routes/Shop/+page.svelte
@@ -1,10 +1,9 @@
 <script>
     import Main from "../../components/Main.svelte";
     import AdBanner from "../../components/AdBanner.svelte";
+    import ShopDash from "../../components/dashborad/ShopDash.svelte";
 </script>
 
 <Main>
-    <AdBanner images={["/ads/ad1.jpg", "/ads/ad2.jpg", "/ads/ad3.jpg"]} />
-
-    <h1>Welcome to CC</h1>
+    <ShopDash />
 </Main>
