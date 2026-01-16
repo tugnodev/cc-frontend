65f4c3159bf3e641b268b05f25b38df3e289e4ff
tugnodev <tugnodev@gmail.com>
2026-01-16 18:25:02 +0000

    shop rectif

---
+++ src/routes/shop/article/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Main from "../../../components/Main.svelte";
+    import Article from "../../../components/Article.svelte";
+</script>
+
+<svelte:head>
+    <title>Shop - Articles</title>
+</svelte:head>
+
+<Main>
+    <!-- <div class="p-6"> -->
+    <h1 class="text-2xl font-bold mb-4">Mes articles</h1>
+    <Article />
+    <!-- </div> -->
+</Main>
