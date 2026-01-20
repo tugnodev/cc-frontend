49ab40d184c845cdf81658d1a3f37786bcad2c43
ebeydi <ebeydif@gmail.com>
2026-01-20 16:18:50 +0000

    uo

--- src/components/Commande.svelte
+++ src/components/Commande.svelte
@@ -156,10 +156,12 @@
     class="w-full flex flex-col items-center justify-start mt-16 mb-24 px-1 transition-all duration-300 ease-in-out"
 >
     <!-- TITRE -->
-    <h1 class="text-2xl font-bold flex items-center gap-2">
-        <ShoppingCart class="w-6 h-6 text-blue-500" />
-        Commandes
-    </h1>
+    <div class="space-y-4 p-4">
+        <h1 class="text-2xl font-bold flex items-center gap-2">
+            <ShoppingCart class="w-6 h-6" />
+            Commandes
+        </h1>
+    </div>
 
     <!-- FILTRE -->
     <div class="flex flex-wrap gap-2">
