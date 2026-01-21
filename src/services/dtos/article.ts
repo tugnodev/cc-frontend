0b11b072c01c9d9d38a17db06c9ece45f98bd7f9
tugnodev <tugnodev@gmail.com>
2026-01-21 18:08:34 +0000

    article store

--- src/services/dtos/article.ts
+++ src/services/dtos/article.ts
@@ -1,34 +1,34 @@
 export interface createArticleDto {
-    userId: string;
-    title: string;
-    images: string[];
-    category: string[];
-    description: string;
-    price: number;
-    stock: number;
+  userId: string;
+  title: string;
+  images: string[];
+  category: string[];
+  description: string;
+  price: number;
+  stock: number;
 }
 
 export interface updateAticleDto {
-    id: string;
-    userId?: string;
-    title?: string;
-    images?: string[];
-    category?: string[];
-    description?: string;
-    price?: number;
-    stock?: number;
+  id: string;
+  userId?: string;
+  title?: string;
+  images?: string[];
+  category?: string[];
+  description?: string;
+  price?: number;
+  stock?: number;
 }
 
 export interface articleDto {
-    id: string;
-    userId: string;
-    title: string;
-    images: string[];
-    category: string[];
-    description: string;
-    price: number;
-    stock: number;
-    views: number;
-    createdAt: Date;
-    updatedAt: Date;
+  id: string;
+  userId: string;
+  title: string;
+  images: string[];
+  category: string[];
+  description: string;
+  price: number;
+  stock: number;
+  views?: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
