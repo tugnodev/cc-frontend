52f00fbd8f15ccab1ad6d8f209085b4d8b1b27b3
DevproCode <diamankamadou9@gmail.com>
2026-01-28 16:04:28 +0000

    chat

--- src/routes/market/chat/+page.svelte
+++ src/routes/market/chat/+page.svelte
@@ -100,6 +100,10 @@ let discussions: Discussion[] = [
             ]
         }
     ];
+    let currentUser = {
+        name: "Moi",
+        avatar: "https://i.pravatar.cc/150?u=my_unique_id" 
+    };
 
     let selectedId: number | null = null;
     let newMessageText = "";
@@ -149,7 +153,7 @@ let discussions: Discussion[] = [
 
     
     {#if !selectedId}
-    <div class="w-full flex flex-col justify-start mt-5 mb-28 px-1 transition-all duration-300 ease-in-out">
+    <div class="w-full flex flex-col justify-start mt-0 mb-28 px-1 transition-all duration-300 ease-in-out">
         <div class="mt-20"> 
             <h1 class="text-2xl font-bold mb-3">Discussions</h1>
             <div class="flex gap-2 mb-4">
@@ -198,8 +202,9 @@ let discussions: Discussion[] = [
         </div>
     </div>
     {:else}
-        <div class="flex  flex-col mt-10 h-[90vh]">
-            <div class="w-full flex  items-center  gap-7 justify-start mt-5 mb-4 px-1 ">
+        <div class="flex  flex-col mt-10 h-[90vh]"> 
+            <div class="flex  flex-col mt-5 bg-base-300/60" >
+            <div class="w-full flex  items-center  gap-7 justify-start mt-1 mb-4 px-1 ">
                 <button 
                     class="btn btn-ghost btn-circle btn-sm mt-10" 
                     on:click={() => selectedId = null}
@@ -217,28 +222,41 @@ let discussions: Discussion[] = [
                     <h2 class="font-bold text-lg">{currentDiscussion?.sender}</h2>
                 </div>
             </div>
-            <hr class="border-base-500 opacity-50" />
+            </div>
+            <!--<hr class="border-base-500 opacity-50" /> -->
 
-            <div class="flex-1 overflow-y-auto p-4 bg-base-200 rounded-box mb-4 no-scrollbar">
-                {#each currentDiscussion.messages as msg}
-                    <div class="chat {msg.sender === 'me' ? 'chat-end' : 'chat-start'} p-2">
-                        {#if msg.sender === 'them'}
-                            <div class="chat-image avatar">
-                                <div class="w-8 rounded-full">
-                                    <img src={currentDiscussion.avatar} alt="" />
-                                </div>
+             <div class="flex-1 overflow-y-auto p-4 bg-base-200 rounded-box mb-4 no-scrollbar">
+            {#each currentDiscussion.messages as msg}
+                <div class="chat {msg.sender === 'me' ? 'chat-end' : 'chat-start'} p-2">
+                    
+                    {#if msg.sender === 'them'}
+                        <div class="chat-image avatar">
+                            <div class="w-8 rounded-full">
+                                <img src={currentDiscussion.avatar} alt={currentDiscussion.sender} />
                             </div>
-                        {/if}
-                        
-                        <div class="chat-bubble">{msg.text}</div>
+                        </div>
+                    {/if}
+
+                    {#if msg.sender === 'me'}
+                        <div class="chat-image avatar">
+                            <div class="w-8 rounded-full">
+                                <img src={currentUser.avatar} alt={currentUser.name} />
+                            </div>
+                        </div>
+                    {/if}
+                    
+                    <div class="chat-bubble {msg.sender === 'me' ? 'chat-bubble-primary' : ''}">
+                        {msg.text}
+                    </div>
+
                         <div class="chat-footer opacity-50 text-xs p-1">
                             {msg.sender === 'me' ? 'Envoyé' : 'Reçu'} à {msg.timestamp.toLocaleTimeString([], {hour:'2-digit', minute:'2-digit'})}
                         </div>
-                    </div>
-                {/each}
-            </div>
+                </div>
+            {/each}
+        </div>
 
-            <div class="join w-full max-w-sm mx-auto ">
+            <div class="join w-full max-w-sm mx-auto  ">
                 <input 
                     type="text" 
                     placeholder="Écrivez votre message..." 
